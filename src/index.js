5e5117104e2ea33c6a7d110e162cf8b868005ef8
shayne-wenzel <shayne.wenzel22@gmail.com>
2022-08-10 14:08:03 +0200

    Add Atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('9b084ff7cd5a492f99c5075765998455').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
